6aecb4f79bcf83a1d20b715ff5d64767b4b7c308
Tae Heon Kim <top1kth98@gmail.com>
2020-02-10 17:10:30 +0900

    3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from "axios";
 // 외부의 데이터를 가져오기 위한 툴, axios
 import Movie from "./Movie";
 import PropTypes from 'prop-types';
+import "./App.css";
+// css적용을 위해 css파일 import
 
 // state (동적으로 변하는 데이터를 처리하기 위함!)
 
@@ -16,7 +18,7 @@ class App extends React.Component{
     };
 
     async componentDidMount(){
-        // axios를 사용해 아래 url에 있는 데이터를 가져온다. (데이터를 온전히 가져올때까지 기다려야 하므로 async, await 쓴다.)
+        // 렌더링을 하고 바로 직후에, axios를 사용해 아래 url에 있는 데이터를 가져온다. (데이터를 온전히 가져올때까지 기다려야 하므로 async, await 쓴다.)
         const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
         // console.log()해보면, 위 json 파일에서 data.data.movies에 실제 영화 데이터들이 들어잇기 때문에 변수를 저렇게 지정해준다.
         this.setState({ movies, isLoading: false});
@@ -28,9 +30,19 @@ class App extends React.Component{
         const {isLoading, movies} = this.state;
         return (
             // 아래 구문은, if (isLoading) {"Loading"} else {movies.map(~~~)}와 같음
-            <div>{isLoading ? "Loading..." : movies.map(arg => {
-                return <Movie key={arg.id} id={arg.id} year={arg.year} title={arg.title} summary={arg.summary} poster={arg.medium_cover_image}/>
-            })}</div>
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map(arg => {
+                            return <Movie key={arg.id} id={arg.id} year={arg.year} title={arg.title} summary={arg.summary} poster={arg.medium_cover_image} genres={arg.genres}/>
+                        })}
+                    </div>
+                )}
+            </section>
         );
     }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
+function Movie({year, title, summary, poster, genres}){
     return (
-        <div>
-            <h5>{title}</h5>
-            <img src={poster} alt={summary}/>
+        <div className="movie">
+            <img src={poster} alt={summary} title={title}/>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__sumamry">{summary.slice(0,140)}...</p>
+                <ul className="movie__genres">
+                    {genres.map((argu, index) => (<li key={index} className="genres__genre">{argu}</li>))}
+                </ul>
+            </div>
         </div>
     )
 }
@@ -16,7 +24,8 @@ Movie.propTypes={
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
