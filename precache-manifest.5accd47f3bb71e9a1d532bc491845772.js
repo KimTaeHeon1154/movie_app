4c339c7c4e5fe3a2b4b4be9dbfa332e17ac10aa7
Tae Heon Kim <top1kth98@gmail.com>
2020-02-12 16:14:53 +0900

    Updates

---
+++ precache-manifest.5accd47f3bb71e9a1d532bc491845772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692a3bd954b5060d4e1ae0b64cb9d4a4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0a58016b76390ca665d8",
+    "url": "/movie_app/static/css/main.7c7f51c8.chunk.css"
+  },
+  {
+    "revision": "f5dfd9032378cd0d46cb",
+    "url": "/movie_app/static/js/2.b0f241b3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie_app/static/js/2.b0f241b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a58016b76390ca665d8",
+    "url": "/movie_app/static/js/main.06c6cd57.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
